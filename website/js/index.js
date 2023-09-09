3113baa8debaeab82252bcd03a4370aa48ceec99
Heonjae Shin <88124365+heonjaes@users.noreply.github.com>
2023-09-10 00:48:26 +1000

    Delete website directory

--- website/js/index.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch(
-        "https://nwkahvo7t43jmmmij2tbsxuklu0eqbmf.lambda-url.ap-southeast-2.on.aws/"
-    );
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
-}
-updateCounter();
